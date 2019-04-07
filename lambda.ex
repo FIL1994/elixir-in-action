37f05dc409796c87c25ebcb6266ecdf606724842
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-06 21:45:51 -0400

    added capture operator

--- lambda.ex
+++ lambda.ex
@@ -8,6 +8,38 @@ defmodule Lambda do
   end
 
   def use_square(a \\ 1) do
-    get_square.(a)
+    square = get_square()
+    square.(a)
+  end
+
+  def print do
+    print_element = fn x -> IO.puts(x) end
+
+    Enum.each(
+      [1, 2, 3],
+      print_element
+    )
+
+    Enum.each(
+      [1, 2, 3],
+      fn x -> IO.puts(x) end
+    )
+  end
+
+  def capture_operator do
+    Enum.each(
+      [1, 2, 3],
+      # the capture operator takes the full function qualifier (module name, function name and arity)
+      # and turns that into a lambda
+      &IO.puts/1
+    )
+  end
+
+  def capture do
+    lambda = fn x, y, z -> x * y + z end
+    # each argument is referred via &n which identifies the nth argument
+    lambda_with_capture = &(&1 * &2 + &3)
+
+    {lambda.(1, 2, 3) === lambda_with_capture.(1, 2, 3), lambda.(1, 2, 3)}
   end
 end
